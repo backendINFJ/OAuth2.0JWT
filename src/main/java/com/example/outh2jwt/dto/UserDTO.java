cac15c7dfefbb52e267217df4648810b819cdda3
yoonjae <backendINFJ@github.com>
2024-06-24 10:35:44 +0900

    Feat : UserDTO 생성

---
+++ src/main/java/com/example/outh2jwt/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.example.outh2jwt.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+
+    private String role;
+    private String name;
+    private String username;
+}
